c9141a8b1c567bae415dde918ce9364b7e9610cb
BaySerious <67412818+BaySerious@users.noreply.github.com>
2020-10-03 22:06:14 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# MÜZİK BOT ALTYAPI
+
+hey sen! bu altyapıyı bulmuşsan...HELAL! 
+Bu altyapı müzik bot altyapısıdır.
+
+# İYİ KULLANIMLAR!
---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+  "ownerID": "KENDİ İDNİZ",//Bu kısıma, kendi ID'niz. 
+  "sahip": ["KENDİ İDNİZ"],
+  "admins": ["YETKİLİ ROL İD"],//Yetkili eklemek isterseniz, o kişinin ID'si.
+  "support": ["DESTEK EKİBİ ROL İD"],//Destek ekibi eklemek isterseniz, o kişinin ID'si.
+  "token": "BOT TOKENİ",//Botun, tokeni.
+  "dashboard" : {
+    "oauthSecret": "",//Client secret.
+    "callbackURL": `https://PROJENİZİN_ADI.glitch.me/callback`,//Site Çalışmıyor
+    "sessionSecret": "AstreaGaming",//sallayın yada ellemeyin
+    "domain": "https://PROJENİZİN_ADI.glitch.me/"//Site Çalışmıyor
+  }
+};
+
+module.exports = config;
